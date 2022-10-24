0714d4b4eb605a42fe65654fc80ddce61fa957f1
arren16 <4rr311@gmail.com>
2022-10-24 17:21:36 +0700

    fix previous wrong commit, task: https://android-203-group07.atlassian.net/browse/A2G-11?atlOrigin=eyJpIjoiNDJjOWEwZWMyZDk0NDI2YjllZmJlMzUwMzU5NjUxODQiLCJwIjoiaiJ9

--- app/src/main/java/com/example/myapplication/FragmentFrame6.java
+++ app/src/main/java/com/example/myapplication/FragmentFrame6.java
@@ -1,14 +1,12 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-
+public class FragmentFrame6 extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_fragment_frame5);
     }
 }
