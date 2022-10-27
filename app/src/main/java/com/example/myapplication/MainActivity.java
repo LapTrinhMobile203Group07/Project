9ddd84a02b19c08368dafb95c096ee51805c93a2
20120568 <20120568@student.hcmus.edu.vn>
2022-10-27 18:22:54 +0700

    frame2.xml và code grid apdapter

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends Activity {
+
+    GridView coursesGV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_fragment_frame2);
+        coursesGV = findViewById(R.id.GridView);
+        ArrayList<Item> courseModelArrayList = new ArrayList<Item>();
+
+        courseModelArrayList.add(new Item("1", R.drawable.avatar01));
+        courseModelArrayList.add(new Item("2", R.drawable.avatar02));
+        courseModelArrayList.add(new Item("3", R.drawable.avatar03));
+        courseModelArrayList.add(new Item("4", R.drawable.avatar04));
+        courseModelArrayList.add(new Item("5", R.drawable.avatar05));
+        courseModelArrayList.add(new Item("6", R.drawable.avatar06));
+        courseModelArrayList.add(new Item("7", R.drawable.avatar06));
+
+        GridApdapter adapter = new GridApdapter(this, courseModelArrayList);
+        coursesGV.setAdapter(adapter);
     }
 }
