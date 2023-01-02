66d6367dfec584b7e02afdf1aadfc9e14063cfe5
Erel <tranducanhghu@gmail.com>
2023-01-02 21:43:37 +0700

    them nut mo camera

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,30 +1,46 @@
 package com.example.myapplication;
+import static androidx.core.content.ContentProviderCompat.requireContext;
+
 import android.Manifest;
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.icu.util.Calendar;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.IntentSenderRequest;
+import androidx.activity.result.contract.ActivityResultContract;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.io.File;
 import java.util.ArrayList;
 
 
@@ -34,13 +50,20 @@ public class MainActivity extends FragmentActivity implements MainCallbacks {
     FooterLayout footerLayout;
     SearchLayout searchLayout;
 
-    
     HomeLayout homeLayout;
     AllPhotosLayout allPhotosLayout;
     AllAlbumLayout allAlbumLayout;
     SpecificAlbumLayout specificAlbumLayout;
     //Tài
     GridView gridPhoto;
+
+    //Đức Anh
+    Context context = this;
+    FloatingActionButton btnCamera;
+    Uri cam_uri;
+
+
+
 
 
     @Override
@@ -58,8 +81,74 @@ public class MainActivity extends FragmentActivity implements MainCallbacks {
         footerLayout = FooterLayout.newInstance();
         ft.replace(R.id.footFrag_holder, footerLayout);
         ft.commit();
-    
+
+
+        // Permission
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            //activityResultLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 001);
+        }
+
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            //activityResultLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 002);
+        }
+
+
+        //Camera button
+        btnCamera = (FloatingActionButton) findViewById(R.id.btnCamera);
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ActivityCompat.checkSelfPermission(context, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(new String[]{Manifest.permission.CAMERA}, 003);
+                }
+                if (ActivityCompat.checkSelfPermission(context, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                    openCamera();
+                }
+            }
+        });
     }
+
+
+    // Yêu cầu: Mở activity camera đồng thời trả về ảnh chụp được từ người dùng
+    // Khó khăn: API mới cập nhật, ko thể sử dụng putExtra và ko thể sử dụng startActivityForResult
+    //           Đồng thời cũng có quá ít hướng dẫn và kiến thức để làm
+    private void openCamera(){
+        Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // Tạo path, file cho ảnh được chụp từ camera
+        File directory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM + "/Camera");
+        if(!directory.exists()){
+            directory.mkdirs();
+        }
+
+        String fileName = "IMG" + System.currentTimeMillis() + ".jpg";
+        File file = new File(directory, fileName);
+        cam_uri = Uri.fromFile(file); // Có thể có vấn đề
+
+        // Lý thuyết cũ, dùng putExtra và startActivityForResult để lưu ảnh, nhưng đã ngừng hỗ trợ
+        //camera_intent.putExtra(MediaStore.EXTRA_OUTPUT, cam_uri);
+
+        // Phải dùng hàm phía dưới nhưng hiện tại ko thể nên thay thế bằng hàm này
+        startActivity(camera_intent);
+
+        // Chắc chắn có vấn đề
+        /*
+        ActivityResultLauncher<Intent> takeCamera = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        //Intent data = result.getData();
+                        //Bitmap photo = (Bitmap) data.getExtras().get("data");
+                    }
+                }
+        );
+        takeCamera.launch(camera_intent);
+        */
+    }
+
 
 
     // MainCallback implementation (receiving messages coming from Fragments)
